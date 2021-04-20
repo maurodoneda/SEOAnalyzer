a8c61840e51dfefee67914f9d23e26ef80a4b118
Mauro Doneda <mauro@silverstonesoftware.co.uk>
2021-04-20 22:35:35 +0100

    add client app

--- Logic/Services/SearchService.cs
+++ Logic/Services/SearchService.cs
@@ -4,13 +4,8 @@ using HtmlAgilityPack;
 using Logic.Services.Interfaces;
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Net;
-using System.Text;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-using System.Web;
 
 namespace Logic.Services
 {
@@ -23,77 +18,52 @@ namespace Logic.Services
             _context = context;
         }
 
-        //public static string GetPosition(string searchTerm, string url)
-        //{
-        //    string raw = "http://www.google.co.uk/search?num=100&q={0}&btnG=Search";
-
-        //    string search = string.Format(raw, HttpUtility.UrlEncode(searchTerm));
-        //    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(search);
-        //    HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-
-        //    using (StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.ASCII))
-        //    {
-        //        string html = reader.ReadToEnd();
-
-        //        return FindPosition(html, url);
-        //    }
-        //}
-
-        //private static string FindPosition(string html, string url)
-        //{
-        //    string lookup = new Regex("<span class=\"BNeawe\\><a href =\"(\\w+[a-zA-Z0-9.-?=/]*)>").ToString();
-
-        //    MatchCollection matches = Regex.Matches(html, lookup);
-        //    string resultPositions = "";
-        //    for (int i = 0; i < matches.Count; i++)
-        //    {
-        //        string match = matches[i].Groups[2].Value;
-        //        if (match.Contains(url))
-        //            resultPositions += $"{i + 1},";
-        //    }
-        //    return resultPositions.TrimEnd(',');
-        //}
-
         public async Task<string> GetGoogleResultsAsync(string searchTerm, string url)
         {
-            var search = new Search
+            Search search = new Search
             {
                 Query = searchTerm,
                 Date = DateTime.Now
             };
 
-            //land + registry + search
+            Result result = new Result()
+            {
+                UrlAnalyzed = url,
+            };
+
             int maxResultPosition = 100;
             searchTerm = searchTerm.Replace(' ', '+');
+
+            if (url.Contains("www"))
+            {
+                url = url.Replace("www.", "").Split('.')[0];
+
+            }
 
             string searchUrl = $"http://www.google.co.uk/search?num={maxResultPosition}&q={searchTerm}";
             // need to process to get the real URL of the question.
 
-            var response = new HtmlWeb().Load(searchUrl);
+            HtmlDocument response = new HtmlWeb().Load(searchUrl);
 
             //resultContent.DocumentNode.SelectNodes("//span[contains(@class, 'BNeawe')]")
-            var nodes = response.DocumentNode.SelectNodes("//div[contains(@class, 'BNeawe UPmit AP7Wnd')]");
-
+            HtmlNodeCollection nodes = response.DocumentNode.SelectNodes("//div[contains(@class, 'BNeawe UPmit AP7Wnd')]");
 
             if (nodes != null)
             {
                 string stringResult = "";
-                var indexes = nodes.Select((x, i) => new { i, x.InnerHtml })
+                IEnumerable<int> indexes = nodes.Select((x, i) => new { i, x.InnerHtml })
                                     .Where(x => x.InnerHtml.Contains(url))
                                     .Select(x => x.i + 1);
 
-                foreach (var i in indexes)
+                foreach (int i in indexes)
                 {
                     stringResult += $"{i},";
                 }
 
                 stringResult = stringResult.TrimEnd(',');
-                var result = new Result()
-                {
-                    UrlAnalyzed = url,
-                    Position = stringResult,
-                };
 
+
+                result.Position = stringResult;
                 search.Result = result;
 
                 _context.Searches.Add(search);
